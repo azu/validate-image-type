030703975bda1e17fbab044d1580a6941c3f8036
azu <azu@users.noreply.github.com>
2020-10-02 09:57:25 +0900

    fix: svg validation without file name (#2)
    * fix: svg checking without file extension .sv
    
    * refactor: define BINARY_READ_LENGTH
    
    * docs: cleanup
    
    * test: add test case for non-svg ext

--- src/validate-image-type.ts
+++ src/validate-image-type.ts
@@ -1,10 +1,18 @@
-import fs from "fs";
-
-import path from "path";
 import assert from "assert";
 import imageType from "image-type";
 import readChunk from "read-chunk";
 import isSvg from "is-svg";
+import * as fs from "fs";
+const BINARY_READ_LENGTH = 24;
+const isBinary = (buffer: Buffer): boolean => {
+    for (let i = 0; i < BINARY_READ_LENGTH; i++) {
+        const characterCode = buffer[i];
+        if (characterCode === 65533 || characterCode <= 8) {
+            return true;
+        }
+    }
+    return false;
+};
 
 export type ValidateImageTypeOptions = {
     /**
@@ -28,27 +36,31 @@ export type ValidateImageResult = {
  * Detect the image type of a Buffer or Uint8Array
  * This check is based on https://github.com/sindresorhus/image-type
  */
-export function validateBufferMIMEType(
-    buffer: Buffer | Uint8Array,
-    options: ValidateImageTypeOptions
-): ValidateImageResult {
+export function validateBufferMIMEType(buffer: Buffer, options: ValidateImageTypeOptions): ValidateImageResult {
     const mimeTypes = options.allowMimeTypes;
     assert.ok(
         Array.isArray(mimeTypes) && mimeTypes.every((mimeType) => mimeType.includes("/")),
         `Should be set an array of mimeType. e.g.) ['image/jpeg']`
     );
-    const result = imageType(buffer);
-    if (!result) {
+    const imageTypeResult = imageType(buffer);
+    if (!imageTypeResult) {
         return {
             ok: false,
-            error: new Error("This buffer is not image"),
+            error: new Error(
+                `This buffer is not supported image. allowMimeTypes: ${JSON.stringify(mimeTypes)}` +
+                    (options.originalFilename ? `, filename: ${options.originalFilename}` : "")
+            ),
         };
     }
-    const isAllowed = mimeTypes.includes(result.mime);
+    const isAllowed = mimeTypes.includes(imageTypeResult.mime);
     if (!isAllowed) {
         return {
             ok: false,
-            error: new Error(`This buffer is disallowed image: ${result.mime}`),
+            error: new Error(
+                `This buffer is disallowed image MimeType: ${imageTypeResult.mime}, allowMimeTypes: ${JSON.stringify(
+                    mimeTypes
+                )}` + (options.originalFilename ? `,filename: ${options.originalFilename}` : "")
+            ),
         };
     }
     return {
@@ -71,24 +83,30 @@ export function validateBufferMIMEType(
  * ```
  */
 export function validateMIMEType(filePath: string, options: ValidateImageTypeOptions): ValidateImageResult {
-    const mimeTypes = options.allowMimeTypes;
-    // Handle SVG as special case
-    // https://github.com/sindresorhus/is-svg
-    const allowSVG = mimeTypes.includes("image/svg+xml");
-    const fileExt = path.extname(options.originalFilename ?? filePath);
-    if (allowSVG && fileExt === ".svg") {
-        const content = fs.readFileSync(filePath, "utf-8");
-        if (!isSvg(content)) {
+    // Use head buffer for performance reason
+    const buffer = readChunk.sync(filePath, 0, BINARY_READ_LENGTH);
+    if (!isBinary(buffer)) {
+        const mimeTypes = options.allowMimeTypes;
+        // Handle SVG as special case
+        // https://github.com/sindresorhus/is-svg
+        const allowSVG = mimeTypes.includes("image/svg+xml");
+        if (allowSVG) {
+            // if the content is not binary, read all content and check it
+            // Note: Require 128 bytes at least one
+            const content = fs.readFileSync(filePath, "utf-8");
+            if (!isSvg(content)) {
+                return {
+                    ok: false,
+                    error: new Error(
+                        `This file is not svg. allowMimeTypes: ${JSON.stringify(mimeTypes)}` +
+                            (options.originalFilename ? `, filename: ${options.originalFilename}` : "")
+                    ),
+                };
+            }
             return {
-                ok: false,
-                error: new Error(`This file is not svg`),
+                ok: true,
             };
         }
-        return {
-            ok: true,
-        };
     }
-    // Use head buffer for performance reason
-    const buffer = readChunk.sync(filePath, 0, 12);
     return validateBufferMIMEType(buffer, options);
 }
--- test/validate-image-type.test.ts
+++ test/validate-image-type.test.ts
@@ -22,25 +22,35 @@ describe("validate-image-type", function () {
         });
         assert.ok(result.ok);
     });
+    it("validate svg images without originalFilename", () => {
+        // without .svg
+        const result = validateMIMEType(path.join(__dirname, "fixtures/svg-but-non-svg-ext"), {
+            allowMimeTypes: ["image/svg+xml"],
+        });
+        assert.ok(result.ok);
+    });
     it("return an error if the images is invalid", () => {
         const result = validateMIMEType(path.join(__dirname, "fixtures/invalid.png"), {
             allowMimeTypes: ["image/png"],
         });
         assert.strictEqual(result.ok, false);
-        assert.strictEqual(result.error?.message, "This buffer is not image");
+        assert.strictEqual(result.error?.message, `This buffer is not supported image. allowMimeTypes: ["image/png"]`);
     });
     it("return an error if the images is not svg", () => {
         const result = validateMIMEType(path.join(__dirname, "fixtures/invalid.svg"), {
             allowMimeTypes: ["image/svg+xml"],
         });
         assert.strictEqual(result.ok, false);
-        assert.strictEqual(result.error?.message, "This file is not svg");
+        assert.strictEqual(result.error?.message, `This file is not svg. allowMimeTypes: ["image/svg+xml"]`);
     });
     it("return an error when the images is not allowed", () => {
         const result = validateMIMEType(path.join(__dirname, "fixtures/valid.png"), {
             allowMimeTypes: [],
         });
         assert.strictEqual(result.ok, false);
-        assert.strictEqual(result.error?.message, "This buffer is disallowed image: image/png");
+        assert.strictEqual(
+            result.error?.message,
+            "This buffer is disallowed image MimeType: image/png, allowMimeTypes: []"
+        );
     });
 });
